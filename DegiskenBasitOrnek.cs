f9a064329e5354c82bc4e113d49ba441bf17a879
Gizem <gizemustahuseyin123@gmail.com>
2023-01-09 19:34:33 +0300

    Add files via upload

---
+++ DegiskenBasitOrnek.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._10._1_Degiskenler
+{
+    class DegiskenBasitOrnek
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 10;
+            Console.WriteLine(sayi);
+            Console.WriteLine(sayi + 2);
+            Console.WriteLine(sayi * sayi);
+            Console.WriteLine(sayi - 5);
+
+            Console.ReadKey();
+        }
+    }
+}
